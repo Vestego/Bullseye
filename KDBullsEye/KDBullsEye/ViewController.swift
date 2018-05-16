c2a38b9c1aaf4a99d9076e3bb51a4f6ffb106c45
Kristopher Devlin <Kris@kristophers-macbook-pro.local>
2018-05-16 13:19:39 +0100

    First time commit

---
+++ KDBullsEye/KDBullsEye/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  KDBullsEye
+//
+//  Created by Kristopher Devlin on 16/05/2018.
+//  Copyright © 2018 Vestego. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        print("hello world")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
